b42e13304b5d1109835f820efebc82db981ef6e6
Nathan Zhou <nathanzhou@outlook.com>
2020-04-28 17:02:14 +1000

    add comments for add

--- cal.py
+++ cal.py
@@ -2,6 +2,12 @@ from random import randint, choice
 
 
 def add():
+    '''Addition.
+
+    Return:
+        5 if correct
+        -3 in incorrect
+    '''
     n1 = randint(2, 1000)
     n2 = randint(2, 1000)
     result = n1 + n2
