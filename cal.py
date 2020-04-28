492be7936790a666c65f8eb9aa3bbac6541fd759
You Zhou <zhouyou.xy@gmail.com>
2020-04-28 16:50:15 +1000

    add mul function
    multiplication

--- cal.py
+++ cal.py
@@ -13,3 +13,17 @@ def add():
         print('incorrect')
         print(f'{n1} + {n2} = {result}')
         return False
+
+
+def mul():
+    n1 = randint(2, 10)
+    n2 = randint(2, 10)
+    result = n1 * n2
+    answer = input(f'{n1} * {n2} = ')
+    if result == int(answer):
+        print('correct')
+        return 3
+    else:
+        print('incorrect')
+        print(f'{n1} * {n2} should be {result}')
+        return -2
